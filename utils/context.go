48cb3c9efc8a9fc937cc88c36afae33613370494
genie <tnals5152@gmail.com>
2023-02-12 02:40:57 +0900

    스타일 변경

--- utils/context.go
+++ utils/context.go
@@ -8,9 +8,10 @@ import (
 	constant "tnals5152.com/api-gateway/const"
 )
 
+// key = config에 저장된 키
 func GetContext(key string) (context.Context, context.CancelFunc) {
-	var timeout time.Duration
-	timeout = GetTimeout(key)
+	// var timeout time.Duration
+	timeout := GetTimeout(key)
 
 	return context.WithTimeout(context.Background(), timeout*time.Second)
 }
