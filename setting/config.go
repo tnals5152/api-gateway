8ef1a38fe3ba3ff7a38027b6da0ea8060d2518bb
genie <tnals5152@gmail.com>
2023-02-12 02:44:33 +0900

    config path 변경가능하도록 수정

--- setting/config.go
+++ setting/config.go
@@ -5,10 +5,16 @@ import (
 	"tnals5152.com/api-gateway/utils"
 )
 
-func SetConfig() {
+func SetConfig(paths ...string) {
+	if len(paths) == 0 {
+		paths = append(paths, "./")
+	}
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
-	viper.AddConfigPath("./") // 안에 path가 배열로 들어가 있어, 여러 경로를 등록해 놓을 수 있다!
+
+	for _, path := range paths {
+		viper.AddConfigPath(path) // 안에 path가 배열로 들어가 있어, 여러 경로를 등록해 놓을 수 있다!
+	}
 
 	if err := viper.ReadInConfig(); err != nil {
 		utils.Panic(err)
