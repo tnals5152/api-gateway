1d22239a9c66ccccc034700566cb39f353084ffe
genie <tnals5152@gmail.com>
2023-04-23 22:01:28 +0900

    단순 proxy 생성 api 추가

--- api/set_proxy/set_proxy.go
+++ api/set_proxy/set_proxy.go
@@ -2,14 +2,27 @@ package setproxy
 
 import (
 	"github.com/gofiber/fiber/v2"
+	host_pkg "tnals5152.com/api-gateway/db/query"
+	"tnals5152.com/api-gateway/handler"
 	"tnals5152.com/api-gateway/model"
 )
 
 func CreateProxy(c *fiber.Ctx) error {
 
-	var path *model.Path
+	var resource *model.Resource
 
-	if err := c.BodyParser(&path); err != nil {
+	if err := c.BodyParser(&resource); err != nil {
+		return c.JSON(err.Error())
+	}
+
+	// 필수 필드 검증 추가
+	if err := resource.Validate(); err != nil {
+		return c.JSON(err.Error())
+	}
+
+	err := handler.SetProxyData(resource)
+
+	if err != nil {
 		return c.JSON(err.Error())
 	}
 
