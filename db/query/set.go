175725c87f62c860e08abe9ec0cca53c4e0cf343
genie <tnals5152@gmail.com>
2023-04-23 22:06:03 +0900

    set proxy query add

---
+++ db/query/set.go
@@ -0,0 +1,60 @@
+package query
+
+import (
+	"errors"
+
+	constant "tnals5152.com/api-gateway/const"
+	"tnals5152.com/api-gateway/model"
+	"tnals5152.com/api-gateway/utils"
+)
+
+func CreateHost(host *model.Host) (id string, err error) {
+	count, err := GetHostCountByName(host.Name)
+
+	if err != nil {
+		return
+	}
+
+	// 이미 동일한 이름의 host가 존재할 때
+	if count != 0 {
+		err = errors.New("이미 일치하는 이름이 존재합니다. 다른 이름을 입력하세요")
+		return
+	}
+
+	id, err = SetHost(host)
+
+	return
+}
+
+func SetHost(host *model.Host) (id string, err error) {
+	ctx, cancel := utils.GetContext(constant.DBTimeout)
+	defer cancel()
+
+	collection, err := GetHostDB()
+
+	if err != nil {
+		return
+	}
+
+	result, err := collection.InsertOne(ctx, host)
+
+	if result != nil {
+		id = utils.GetObjectId(result.InsertedID)
+	}
+
+	return
+}
+
+// proxy document add
+func SetProxy(resource *model.Resource) (err error) {
+
+	collection, err := GetCollection(constant.PATH_COLLECTION)
+
+	if err != nil {
+		return
+	}
+
+	err = collection.SetOne(resource)
+
+	return
+}
---
+++ handler/set_proxy_handler.go
@@ -0,0 +1,15 @@
+package handler
+
+import (
+	"tnals5152.com/api-gateway/db/query"
+	"tnals5152.com/api-gateway/model"
+	util_error "tnals5152.com/api-gateway/utils/error"
+)
+
+func SetProxyData(resource *model.Resource) (err error) {
+	// 0. err에 wrap을 사용하여 에러가 발생한 위치를 저장
+	defer util_error.DeferWrap(&err, 1)
+
+	err = query.SetProxy(resource)
+	return
+}
